bd165fde03b99eb810ced5c24b72264b2e294414
hiriorima <g2117001@fun.ac.jp>
2018-06-11 03:00:24 +0900

    refactor: 尻尾モータの駆動指示classの追加

---
+++ RunMethods/TailCommander.h
@@ -0,0 +1,43 @@
+#include "TailMotorDriver.h"
+
+class TailCommander {
+
+private:
+    TailMotorDriver *motor;
+
+public:
+    TailCommander();
+    
+    /**
+     * rotate - 腕を動かす
+     *
+     * @param  {int32_t}     angle	角度 [deg] 
+     * @return {void} 
+     */
+    void rotate(int32_t angle);
+
+    /**
+     * rotateDefault - デフォルトの角度(コース走行する角度)にする
+     *
+     * @param  {void}
+     * @return {void} 
+     */
+    void rotateDefault();
+
+    /**
+     * rotateExtend - 尻尾を伸ばす
+     *
+     * @param  {void}
+     * @return {void} 
+     */    
+    void rotateExtend();
+
+    /**
+     * reset - モータリセット  
+     *
+     * @param  {void}
+     * @return {void}
+     */
+    void reset();
+    virtual ~TailCommander();
+};
