0c46b4486b105490c964450b5ee9bb9928437b1a
hiriorima <g2117001@fun.ac.jp>
2018-09-10 17:45:29 +0900

    feat: 斜め駐車(灰色検知，ライントレース 未チューニング)

--- RunMethods/Status/Parking.cpp
+++ RunMethods/Status/Parking.cpp
@@ -23,8 +23,14 @@ void Parking::setNextState(){
 }
 
 void Parking::createCourseL(){
+    /*斜め駐車*/
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, GRAYLINE, 4, 0.9, 0.01));
+    patterns.push_back(new RunPattern(LINE_TRACE, 15, DISTANCE, 10, 0.9, 0.01, 0.0, 70));
+    patterns.push_back(new RunPattern(SPIN, 50, DIRECTION, 30));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 55));
+    patterns.push_back(new RunPattern(SPIN, 50, DIRECTION, 90));
 }
 
 void Parking::createCourseR(){
-
+    /*直角駐車*/
 }
