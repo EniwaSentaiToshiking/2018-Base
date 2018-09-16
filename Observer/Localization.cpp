81c477265dc7f781eb82b1df76a14cd99bc5eab7
Toshiki Ikda <b1013001@fun.ac.jp>
2018-09-17 06:45:32 +0900

    自己位置修正

--- Observer/Localization.cpp
+++ Observer/Localization.cpp
@@ -24,8 +24,7 @@ void Localization::update()
 
     D = (TR + TL) / 2; //車体の進んだ距離
 
-    //w = (TR - TL) / (2 * d); //旋回角度計算
-    w = (TR - TL) / d;
+    w = (TR - TL) / (2 * d); //旋回角度計算
 
     x1 = x0 + (D * cos(z0 + (w / 2)));
     y1 = y0 + (D * sin(z0 + (w / 2))); //現在の座標を求める
@@ -34,8 +33,8 @@ void Localization::update()
     point_x = x1;
     point_y = y1; //現在の座標を実際に取得する値
     current_distance += D;
-    //current_direction += (TR - TL)/d*360/(4.0*M_PI);
-    current_direction = z1;
+    current_direction += w*(180/M_PI);
+    //current_direction = z1;
 
     x0 = x1;
     y0 = y1;
