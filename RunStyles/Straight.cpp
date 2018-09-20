23c0c62124f209f1f2b4cb46e079383fe2b572cf
b1013251 <b1013251@gmail.com>
2018-09-20 21:23:14 +0900

    直進制御

--- RunStyles/Straight.cpp
+++ RunStyles/Straight.cpp
@@ -4,6 +4,10 @@ Straight::Straight(int speed){
     wheelInfo = new WheelInfo();
     this->speed = speed;
     pid = new PID(0,0,0);
+
+    if(speed==10) {
+        pid = new PID(2.5, 0.0, 0.01);
+    }
 }
 
 Straight::~Straight(){
@@ -15,5 +19,8 @@ void Straight::init(){
 
 int Straight::getTurnValue(){
     int32_t *info = wheelInfo->getCount();
-    return pidController->getTurn(this->pid, (info[0] - beginCount[0]) - (info[1] - beginCount[1]), 0, 100);
+    if(this->speed > 0)
+        return -pidController->getTurn(this->pid, (info[0] - beginCount[0]) - (info[1] - beginCount[1]), 0, 100);
+    if(this->speed <= 0)
+        return pidController->getTurn(this->pid, (info[0] - beginCount[0]) - (info[1] - beginCount[1]), 0, 100);
 }
