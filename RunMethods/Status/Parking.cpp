7046ee18bcfede6542715cbde6e28f42f092e775
b1013251 <b1013251@gmail.com>
2018-09-20 21:52:43 +0900

    駐車場変更

--- RunMethods/Status/Parking.cpp
+++ RunMethods/Status/Parking.cpp
@@ -22,12 +22,15 @@ void Parking::setNextState(){
     nextState = STOP;
 }
 
+
 void Parking::createCourseL(){
-    patterns.push_back(new RunPattern(LINE_TRACE, 15, GRAYLINE, 55, 1.3, 0.0, 0.02, 40, LEFT));
-    patterns.push_back(new RunPattern(STRAIGHT, 15, DISTANCE, 2));
+     patterns.push_back(new RunPattern(LINE_TRACE, 15, GRAYLINE, 55, 1.5, 0.0, 0.03, 40, LEFT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 10, DISTANCE, 5, 1.5, 0.0, 0.05, 70, RIGHT));
     patterns.push_back(new RunPattern(SPIN, 40, DIRECTION, 30, DIRECTION_LEFT));
+    patterns.push_back(new RunPattern(BRAKE, 0, CLOCK, 500));
     patterns.push_back(new RunPattern(STRAIGHT, 30, DISTANCE, 50));
     patterns.push_back(new RunPattern(TURNING, 50, DIRECTION, -57, DIRECTION_RIGHT));
+    
 }
 
 void Parking::createCourseR(){
